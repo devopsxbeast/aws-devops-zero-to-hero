e143cc6736beb8f32fed0bb4db8f5aef0f0a4c65
devopsxbeast <thodahatke0@gmail.com>
2025-06-04 14:13:34 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,5 @@ set -e
 docker pull devopsxbeast09/simple-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 devopsxbeast09/simple-python-app
+docker run -d -p 5000:5000 devopsxbeast09/simple-python-app  
+  
